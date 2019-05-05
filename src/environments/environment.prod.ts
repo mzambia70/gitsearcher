67bf2df6dedbcd35d671b8a4bb26cf5cedd4ca7a
mzambia70 <mzambia70@gmail.com>
2019-05-06 01:42:29 +0300

    added the git hub apikey

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
-export const environment = {
-  production: true
+export const Environnment = {
+  production: true,
+  apikey: ("b926691cdc0dfc71e3900ab8db6bc9b543d46ac0")
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,4 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-export const environment = {
-  production: false
+export const Environment = {
+  production: false,
+  apikey: ("b926691cdc0dfc71e3900ab8db6bc9b543d46ac0")
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
