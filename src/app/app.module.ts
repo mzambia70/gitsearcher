218d9648d31c05e85d2ea96bd3ddf20a27b36fd5
mzambia70 <mzambia70@gmail.com>
2019-05-06 01:26:01 +0300

    modified app module with more functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubFormComponent } from './github-form/github-form.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { GithubRepoComponent } from './github-repo/github-repo.component';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    GithubFormComponent,
+    GithubProfileComponent,
+    GithubRepoComponent,
+    DateCountPipe,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+
+
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
