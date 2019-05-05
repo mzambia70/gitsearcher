fbc29b50663ae870a26b317ceb55beb469b74e40
mzambia70 <mzambia70@gmail.com>
2019-05-06 01:54:05 +0300

    added the repo  properties for git

---
+++ src/app/github-repo/github-repo.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { UserRequestService } from '../user-http/user-request.service';
+import { Repo } from '../repo-class/repo';
+
+@Component({
+  selector: 'app-github-repo',
+  templateUrl: './github-repo.component.html',
+  providers: [UserRequestService],
+  styleUrls: ['./github-repo.component.css']
+})
+export class GithubRepoComponent implements OnInit {
+
+  @Input() repo:Repo;
+
+  constructor(private userService: UserRequestService) { }
+
+  ngOnInit() {
+  }
+
+}
